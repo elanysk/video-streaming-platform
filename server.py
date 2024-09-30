b7259f18a8e679dc71386f793d3c98e848f94788
patrick muller <patrick42702@gmail.com>
2024-09-30 14:39:26 -0400

    JWT key verification adduser successfully creates a jwt token to save to the user in the database, and verify correctly checks the passed query param against what is stored in the database.

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from email_validator import validate_email
 from flask_mail import Mail, Message
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 DOMAIN = "esk-pj-airplanes.cse356.compas.cs.stonybrook.edu"
 
@@ -10,12 +16,18 @@ app = Flask(__name__)
 client = MongoClient("localhost", 27017)
 db = client.eskpj_airplanes
 
+#config env variables
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 # configure mail server
 # our DNS is esk-pj-airplanes.cse356.compas.cs.stonybrook.edu
 app.config['MAIL_SERVER'] = 'localhost'
 app.config['MAIL_PORT'] = 25
 app.config['MAIL_DEFAULT_SENDER'] = f"root@{DOMAIN}"
 mail = Mail(app)
+
+# configure JWTManager
+jwt = JWTManager(app)
 
 # example route that contacts database. Users is a collection in the eskpj_airplanes database
 @app.route('/data')
@@ -40,12 +52,13 @@ def add_user():
             existing_user = users.find_one({"email": email})
             if existing_user:
                 raise Exception("User already exists")
-            verify_key = 1
+            verify_key = create_access_token(identity=email)
             users.insert_one({"username": username,
                               "password": password,
                               "email": email,
                               "validated": False,
-                              "verify-key": 1})
+                              "verify-key": verify_key})
+            print(f"this is my verify key {verify_key}")
             msg = Message(subject="Verify email", 
                           recipients=[email], 
                           body=f"Please verify your email at http://{DOMAIN}?email={email}&?verify={verify_key}")
@@ -55,9 +68,22 @@ def add_user():
         return jsonify(str(e.args[0])), 400
 
 # He specifies to use query string params for the verify endpoint
-@app.route('/verify')
+@app.route('/verify', methods=['GET'])
 def verify():
-    return "Verify"
+    users = db.users
+    email = request.args["email"]
+    verify_key = request.args["verify"]
+    try:
+        user = users.find_one({"email": email})
+        saved_token = user["verify-key"]
+        if saved_token == verify_key:
+            users.update_one({"email": email}, {"$set": {"validated": True}})
+            return jsonify("Email Successfully Verified")
+        else:
+            raise Exception("Invalid verification key")
+        return
+    except Exception as e:
+        return jsonify(str(e.args[0])), 400
 
 @app.route('/login', methods=['POST'])
 def login():
