cea0c19d6d18ff970940239e99a3f57ac0b32da1
patrick muller <patrick42702@gmail.com>
2024-10-01 10:43:15 -0400

    Implementing front end Attempted to implement front end for the project using flask as well, going to swap to a separate front end for the sake of cleanliness

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, render_template, url_for, redirect
 from pymongo import MongoClient
 from email_validator import validate_email
 from flask_mail import Mail, Message
@@ -10,6 +10,7 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 DOMAIN = "esk-pj-airplanes.cse356.compas.cs.stonybrook.edu"
+SUBMIT_ID = "66d216517f77bf55c5005074"
 
 # configure flask app and pymongo
 app = Flask(__name__)
@@ -29,43 +30,34 @@ mail = Mail(app)
 # configure JWTManager
 jwt = JWTManager(app)
 
-# example route that contacts database. Users is a collection in the eskpj_airplanes database
-@app.route('/data')
-def get_data():
-    users = db.users
-    data = users.find_one({"name": "patrick"})
-    result = [{"_id": str(item[0]), "name": str(item[1])} for item in data.items()]
-    print(result)
-    return jsonify(result)
-
 # for now get params via a POST form. Adjust when we have an answer
 # from ferdman on how to get params
-@app.route('/adduser', methods=['POST'])
+@app.route('/adduser', methods=['GET', 'POST'])
 def add_user():
-    users = db.users
-    print(request.json)
-    username = request.json['username']
-    password = request.json['password']
-    email = request.json['email']
-    try:
-        if username and password and validate_email(email):
-            existing_user = users.find_one({"email": email})
-            if existing_user:
-                raise Exception("User already exists")
-            verify_key = create_access_token(identity=email)
-            users.insert_one({"username": username,
-                              "password": password,
-                              "email": email,
-                              "validated": False,
-                              "verify-key": verify_key})
-            print(f"this is my verify key {verify_key}")
-            msg = Message(subject="Verify email", 
-                          recipients=[email], 
-                          body=f"Please verify your email at http://{DOMAIN}?email={email}&?verify={verify_key}")
-            mail.send(msg)
-        return "User added successfully. Please verify email"
-    except Exception as e:
-        return jsonify(str(e.args[0])), 400
+    if request.method == 'POST':
+        users = db.users
+        username = request.json['username']
+        password = request.json['password']
+        email = request.json['email']
+        try:
+            if username and password and validate_email(email):
+                existing_user = users.find_one({"username": username})
+                if existing_user:
+                    raise Exception("User already exists")
+                verify_key = create_access_token(identity=email)
+                users.insert_one({"username": username,
+                                  "password": password,
+                                  "email": email,
+                                  "validated": False,
+                                  "verify-key": verify_key})
+                msg = Message(subject="Verify email", 
+                              recipients=[email], 
+                              body=f"Please verify your email at http://{DOMAIN}?email={email}&?verify={verify_key}")
+                mail.send(msg)
+            return "User added successfully. Please verify email"
+        except Exception as e:
+            return jsonify(str(e.args[0])), 400
+    return render_template("adduser.html")
 
 # He specifies to use query string params for the verify endpoint
 @app.route('/verify', methods=['GET'])
@@ -85,17 +77,35 @@ def verify():
     except Exception as e:
         return jsonify(str(e.args[0])), 400
 
-@app.route('/login', methods=['POST'])
-def login():
-    users = db.users
-    try:
-        if request.form['username'] and request.form['password']:
-            users.insert_one({"username": request.form['username'],
-                              "password": request.form['password']})
+@app.route('/please_verify', methods=['GET'])
+def please_verify():
+    return render_template("verify.html")
 
-        return "User added"
-    except Exception as e:
-        return e
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        users = db.users
+        username = request.json['username']
+        password = request.json['password']
+        try:
+            if username and password:
+                user = users.find_one({"username": username})
+            else:
+                raise Exception("username not found")
+            if user["password"] == password:
+                # response = make_response(render_template("verify.html"))
+                print("inputted correct password")
+                response = make_response(redirect(url_for("please_verify")))
+                access_token = create_access_token(identity=username)
+                response.set_cookie("session_id", access_token)
+                response.headers["X-CSE356"] = SUBMIT_ID
+                print(response.response)
+                return response
+            else:
+                raise Exception("Invalid password")
+        except Exception as e:
+            return jsonify(str(e.args[0])), 400
+    return render_template("login.html")
 
 @app.route('/logout')
 def logout():
