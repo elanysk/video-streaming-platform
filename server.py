d51e8bb5ff1baadca38b260ff8324f647e3f2db4
Patrick42702 <patrick42702@yahoo.com>
2024-10-07 11:41:23 -0400

    removed spaces from error messages

--- server.py
+++ server.py
@@ -48,9 +48,9 @@ mail = Mail(app)
 def error(err_msg, weird_case=None):
     print(f"found an error: {err_msg}")
     traceback.print_exc()
-    resp = make_response(jsonify({"status": "ERROR", "error":True, "message": err_msg}), 200)
+    resp = make_response(jsonify({"status":"ERROR","error":True,"message": err_msg}), 200)
     if weird_case == "media":
-        resp = make_response('{"status": "ERROR","error":true,"message":"error"}', 200) # more silly spaces?
+        resp = make_response('{"status":"ERROR","error":true,"message":"error"}', 200) # more silly spaces?
     resp.headers["X-CSE356"] = SUBMIT_ID
     return resp
 
