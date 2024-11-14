c6efbbea2d01e42fb3e5780c51857c89a0655374
elanysk <elanysk@gmail.com>
2024-11-14 00:57:42 -0500

    fudge likes

--- bp/collaborative_filtering.py
+++ bp/collaborative_filtering.py
@@ -74,6 +74,10 @@ class CollaborativeFiltering:
         return predicted_matrix
 
     def get_top_recommendations(self, user_id, watched_video_ids, k):
+        all_videos = [vid for vid in db.videos.find()]
+        sorted_videos = sorted(all_videos, key=lambda vid: len(vid['likes']), reverse=True)
+        return [str(vid['_id']) for vid in sorted_videos[:k]]
+
         processing_videos = [str(vid['_id']) for vid in db.videos.find({'status':'processing'})]
 
         if len(watched_video_ids) == 0: # we don't know their preferences
