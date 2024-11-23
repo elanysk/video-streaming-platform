f036586f82af7a80be42c1659f3c4309bf4cfc14
Patrick Muller <patrick42702@gmail.com>
2024-11-23 15:06:25 -0500

    Added gunicorn so that flask runs on 4 workers instead of 1

--- flask.config.js
+++ flask.config.js
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'server',
-      script: './server.py',
-      interpreter: './venv/bin/python',
-      watch: false,  // Optional: Enable if you want pm2 to watch for file changes
-      autorestart: true,  // Optional: Enable auto-restart if the app crashes
+      script: './venv/bin/gunicorn',
+      args: 'server:app --bind 127.0.0.1:5050 --workers 4',
+      interpreter: 'none',  // Prevent PM2 from using another interpreter since Gunicorn is executable
+      watch: false,         // Optional: Enable if you want pm2 to watch for file changes
+      autorestart: true,    // Optional: Restart app if it crashes
     },
   ],
 };
