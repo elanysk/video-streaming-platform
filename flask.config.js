033630be03d82c09bf89b3f7d45f397f14683da2
Patrick Muller <patrick42702@gmail.com>
2024-11-23 15:11:18 -0500

    bind gunicorn on all interfaces

--- flask.config.js
+++ flask.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'server',
       script: './venv/bin/gunicorn',
-      args: 'server:app --bind 127.0.0.1:5050 --workers 4',
+      args: 'server:app --bind 0.0.0.0:5050 --workers 4',
       interpreter: 'none',  // Prevent PM2 from using another interpreter since Gunicorn is executable
       watch: false,         // Optional: Enable if you want pm2 to watch for file changes
       autorestart: true,    // Optional: Restart app if it crashes
