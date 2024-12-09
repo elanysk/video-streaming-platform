40f7205ff61c0f1a1b589daf88fff1e70f1c29dd
elanysk <elanysk@gmail.com>
2024-12-09 12:53:52 -0500

    debug

--- bp/collaborative_filtering.py
+++ bp/collaborative_filtering.py
@@ -15,17 +15,19 @@ class CollaborativeFiltering:
         self.con = redis.Redis(host=REDIS_IP, decode_responses=True)
         # TODO: uncomment
         # self.con.delete('likes', 'like_count', 'video_ids', 'u2i', 'v2i', 'num_users', 'num_videos')
-        # users = list(db.users.find({}))
-        # videos = list(db.videos.find({}))
-        # video_ids = [str(video['_id']) for video in videos]  # String ID
-        # u2i = {str(doc['_id']): idx for idx, doc in enumerate(users)}  # String ID
-        # v2i = {str(doc['_id']): idx for idx, doc in enumerate(videos)}  # String ID
-        # for video in videos: self.con.hset('like_count', str(video['_id']), '0')
-        # self.con.rpush('video_ids', *video_ids)
-        # self.con.hset('u2i', mapping=u2i)
-        # self.con.hset('v2i', mapping=v2i)
-        # self.con.set('num_users', len(users))
-        # self.con.set('num_videos', len(videos))
+        self.con.delete('video_ids', 'u2i', 'v2i', 'num_users', 'num_videos')
+        users = list(db.users.find({}))
+        videos = list(db.videos.find({}))
+        video_ids = [str(video['_id']) for video in videos]  # String ID
+        u2i = {str(doc['_id']): idx for idx, doc in enumerate(users)}  # String ID
+        v2i = {str(doc['_id']): idx for idx, doc in enumerate(videos)}  # String ID
+        for video in videos: self.con.hset('like_count', str(video['_id']), '0')
+        self.con.rpush('video_ids', *video_ids)
+        self.con.hset('u2i', mapping=u2i)
+        self.con.hset('v2i', mapping=v2i)
+        self.con.set('num_users', len(users))
+        self.con.set('num_videos', len(videos))
+
 
     def build_matrix(self, likes, u2i, v2i):
         M = np.zeros((len(u2i), len(v2i)), dtype=np.int8)
